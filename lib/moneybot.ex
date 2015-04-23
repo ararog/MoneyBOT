6fde6125ecef989e8415d672f3b612bffa323bd0
Rogério Pereira Araújo <rogerio.araujo@gmail.com>
2015-04-23 14:54:58 -0300

    Fixing port number

--- lib/moneybot.ex
+++ lib/moneybot.ex
@@ -29,7 +29,7 @@ defmodule MoneyBOT do
     ])
     { :ok, _ } = :cowboy.start_http(:http,
                                     100,
-                                   [{:port, 80}],
+                                   [{:port, 8080}],
                                    [{ :env, [{:dispatch, dispatch}]}]
                                    )
   end
