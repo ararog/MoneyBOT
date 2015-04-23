2d85b554568b12b455fe53a9e4df228be1bde758
Rogério Pereira Araújo <rogerio.araujo@gmail.com>
2015-04-23 10:55:02 -0300

    Fixing money quote requets handling

--- lib/moneybot.ex
+++ lib/moneybot.ex
@@ -1,2 +1,34 @@
 defmodule MoneyBOT do
+
+  def start(_type, _args) do
+
+    # Compile takes as argument a list of tuples that represent hosts to
+    # match against.So, for example if your DNS routed two different
+    # hostnames to the same server, you could handle requests for those
+    # names with different sets of routes. See "Compilation" in:
+    #      http://ninenines.eu/docs/en/cowboy/HEAD/guide/routing/
+    dispatch = :cowboy_router.compile([
+
+      # :_ causes a match on all hostnames.  So, in this example we are treating
+      # all hostnames the same. You'll probably only be accessing this
+      # example with localhost:8080.
+      { :_,
+
+        # The following list specifies all the routes for hosts matching the
+        # previous specification.  The list takes the form of tuples, each one
+        # being { PathMatch, Handler, Options}
+        [
+
+          # Serve a dynamic page with a custom handler
+          # When a request is sent to "/", pass the request to the custom handler
+          # defined in module QuoteHandler.
+          {"/", QuoteHandler, []},
+      ]}
+    ])
+    { :ok, _ } = :cowboy.start_http(:http,
+                                    100,
+                                   [{:port, 8080}],
+                                   [{ :env, [{:dispatch, dispatch}]}]
+                                   )
+  end
 end
---
+++ lib/quote_handler.ex
@@ -0,0 +1,61 @@
+
+# A cowboy handler for serving a single dynamic wepbage. No templates are used; the
+# HTML is all generated within the handler.
+defmodule QuoteHandler do
+
+  # We are using the plain HTTP handler.  See the documentation here:
+  #     http://ninenines.eu/docs/en/cowboy/HEAD/manual/cowboy_http_handler/
+  #
+  # All cowboy HTTP handlers require an init() function, identifies which
+  # type of handler this is and returns an initial state (if the handler
+  # maintains state).  In a plain http handler, you just return a
+  # 3-tuple with :ok.  We don't need to track a  state in this handler, so
+  # we're returning the atom :no_state.
+  def init(_type, req, []) do
+    {:ok, req, :no_state}
+  end
+
+
+  # In a cowboy handler, the handle/2 function does the work. It should return
+  # a 3-tuple with :ok, a request object (containing the reply), and the current
+  # state.
+  def handle(request, state) do
+    # construct a reply, using the cowboy_req:reply/4 function.
+    #
+    # reply/4 takes three arguments:
+    #   * The HTTP response status (200, 404, etc.)
+    #   * A list of 2-tuples representing headers
+    #   * The body of the response
+    #   * The original request
+    { :ok, reply } = :cowboy_req.reply(
+
+      # status code
+      200,
+
+      # headers
+      [ {"content-type", "text/plain"} ],
+
+      # body of reply.
+      build_body(request),
+
+      # original request
+      request
+    )
+
+    # handle/2 returns a tuple starting containing :ok, the reply, and the
+    # current state of the handler.
+    {:ok, reply, state}
+  end
+
+  # Termination handler.  Usually you don't do much with this.  If things are breaking,
+  # try uncommenting the output lines here to get some more info on what's happening.
+  def terminate(reason, request, state) do
+    :ok
+  end
+
+
+  def build_body(request) do
+
+  end
+
+end
